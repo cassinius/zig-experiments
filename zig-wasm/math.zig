d2fdb5f6e0a205234a90f41056f0526321188ca4
Bernd Malle <bernd.malle@gmail.com>
2022-11-06 12:25:28 +0100

    added wasm32-wasi as compile target -> works!

--- zig-wasm/math.zig
+++ zig-wasm/math.zig
@@ -1,4 +1,4 @@
-// const std = @import("std");
+const std = @import("std");
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
@@ -8,11 +8,11 @@ export fn square(n: i32) i32 {
     return n * n;
 }
 
-// pub fn main() !void {
-//     const a = 10;
-//     const b = 20;
+pub fn main() !void {
+    const a = 10;
+    const b = 20;
 
-//     const result = add(a, b);
+    const result = add(a, b);
 
-//     std.debug.print("{d} + {d} = {d}", .{a, b, result});
-// }
+    std.debug.print("{d} + {d} = {d}", .{a, b, result});
+}
