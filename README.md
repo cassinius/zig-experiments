1fe7ef80836fe82e7421dca9b6b2f28efd1b5cf3
Bernd Malle <bernd.malle@gmail.com>
2022-11-06 20:02:09 +0100

    started a new http-server project, took a few notes, put zig-cache & zig-out into .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # zig-experiments
 my life has been a constant zig-zag, so why not give this one a shot!?
+
+## Basic insights
+* In Zig, the `.` means anonymous (`.{}` = Anonymous struct, `myStruct{}` = struct with name / type 'myStruct')
+* In Zig, every type is also a value (u8 is a value... hmmm...)
+* 
---
+++ http-server/src/main.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+const net = std.net;
+const StreamServer = net.StreamServer;
+const Address = net.Address;
+
+/// Async Webserver : TCP Listener + HTTP protocol + handlers
+/// Two ways of using it:
+/// 1. Library => create a webserver with config
+/// 2. Executable => gets a config file in YAML
+pub fn main() anyerror!void {
+    try printStuff();
+
+    StreamServer.init(.{});
+}
+
+
+fn printStuff() anyerror!void {
+    std.log.info("Blahoo!", .{});
+    std.log.warn("Omg, iceberg ahead!", .{});
+    std.log.err("We're sinking {s} help in sight!!", .{"NO"});
+    std.log.debug("Lamest debug info ever...", .{});
+}
+
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+//     try bw.flush(); // don't forget to flush!
+// }
+
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
